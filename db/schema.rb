3049b7bb928658c213abf62cef28d160ebc23cb9
cicatriz <ryan@geknowm.com>
2014-08-16 07:11:41 -0400

    convert to pg and unicorn

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140816105250) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: true do |t|
     t.integer  "user_id"
     t.string   "url"
@@ -23,7 +26,7 @@ ActiveRecord::Schema.define(version: 20140816105250) do
     t.datetime "sent_at"
   end
 
-  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -40,7 +43,7 @@ ActiveRecord::Schema.define(version: 20140816105250) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
