5cbb365956f0a038845a0b48d110fc1413c4181e
cicatriz <ryan@geknowm.com>
2014-08-16 06:02:16 -0400

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HardReader::Application.config.secret_key_base = '880ff909763b54b5ffe2551b7141e0ffb2ec5213c843f5b5df860a2e7d4c02b13e34a597ce4728db378d3c4e351f2bc7d81abcc443fd1b01486291b00d48fddf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HardReader::Application.config.session_store :cookie_store, key: '_hard_reader_session'
