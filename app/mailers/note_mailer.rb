624f44fddf40a4d31da02bdbf0dae08233c5666b
cicatriz <ryan@geknowm.com>
2014-08-17 11:27:30 -0400

    fix html/text generation

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -1,12 +1,14 @@
 class NoteMailer < ApplicationMailer
+  include ActionView::Helpers::SanitizeHelper
+
   def to_read_email(note)
     @note = note
 
     mail(from: "Hard Reader <#{note.email_route}>",
          to: note.user_email,
          subject: "For you to read",
-         html: generate_template,
-         text: generate_template.strip_tags)
+         html: generate_html,
+         text: generate_text)
   end
 
   def review_email(user)
@@ -14,16 +16,21 @@ class NoteMailer < ApplicationMailer
 
     @user = user
 
-    mail(from: "Hard Reader <#{Note::REVIEW_EMAIL_ROUTE}>",
-         to: user.email,
-         subject: "Your review from Hard Reader",
-         html: generate_template,
-         text: generate_template.strip_tags)
+    out = mail(from: "Hard Reader <#{Note::REVIEW_EMAIL_ROUTE}>",
+               to: user.email,
+               subject: "Your review from Hard Reader",
+               html: generate_html,
+               text: generate_text)
 
     user.notes_reviewable.update_all(sent_at: Time.now.utc)
+    return out
   end
 
-  def generate_template
+  def generate_html
     @html ||= collect_responses({})[0][:body]
+  end
+
+  def generate_text
+    @text ||= strip_tags(generate_html)
   end
 end
